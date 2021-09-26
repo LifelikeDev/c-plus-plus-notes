313bddd95d320169a2102e5f79f8fd9b3776aa00
Navian <primeguyy@gmail.com>
2021-09-26 20:11:05 +0000

    anonymous enumeration types

--- enum_types.cpp
+++ enum_types.cpp
@@ -7,13 +7,15 @@ int main()
     // Enumeration type refers to a list of user-defined symbolic constants which
     // represent integral values
     
+    // enum Choice {Yes, No};
+
     enum Choice {Yes, No};
     // order matters as the first in the list reps. 0 and the second reps 1
     // {No, Yes } (0, 1) is not the same as {Yes, No} (0, 1)
     
-    Choice value = Yes;
+    // Choice value = Yes;
     
-    while (value) {
+    while (Yes) {
         cout << "Response code: YES" << endl;
     }
 }
