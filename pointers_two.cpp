a15736caf1cfbc5d7cb388f26b660a7bb0139031
Navian <primeguyy@gmail.com>
2021-09-26 19:43:54 +0000

    pointer operators illustrated

---
+++ pointers_two.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // pointers
+    // a pointer is a variable that stores the address of another variable (in memory)
+    // * = indirection operator; allows access through a pointer; initializes a pointer
+    // & = address operator; reads the address of a variable
+    
+    int value;
+    
+    cout << "Enter any number between 10 and 100: ";
+    cin >> value;
+    
+    int *valuePointer = &value;
+    
+    cout << value << " " << *valuePointer;
+    
+    return 0;
+}
