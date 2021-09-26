72451c286ed1fff1c3f25a227767640f0345e5f5
Navian <primeguyy@gmail.com>
2021-09-26 18:59:41 +0000

    variable declaration and initialization

---
+++ variable_declaration.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int num;
+    char letter;
+    double money;
+    
+    num = 23;
+    letter = 'I';
+    money = 5635.93;
+    
+	// variable initialization
+    int anotherChar {'B'};
+    float weight {212.2345};
+    
+    cout << num << endl;
+    cout << letter << endl;
+    cout << money << endl;
+    cout << anotherChar << endl;
+    cout << weight << endl;
+    
+    return 0;
+}
